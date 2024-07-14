63cb2f70037b753489a2b117d780f5aff06c4ddc
antoinebtn <antoinebtn@gmail.com>
2024-07-14 18:45:09 +0200

    searchBar component

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component,  } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from "../search-bar/search-bar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ 
+  imports: [
     CommonModule,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
-  ],
+    RouterLinkActive,
+    SearchBarComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
